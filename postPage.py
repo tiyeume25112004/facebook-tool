bb3db8e081dcd766c2979c0b8e19c61f0cac6d23
Spycio.Kon <77606941+tiyeume25112004@users.noreply.github.com>
2024-01-16 23:10:42 +0700

    Update postPage.py

--- postPage.py
+++ postPage.py
@@ -1,6 +1,6 @@
 import requests
 GROUP_ID = "1115299825880024"
-TOKEN = "EABiLaWq20EwBOya4HpGsFujVyZBpolomt88B0R4bmLeZBHxYulE4B33vQQ9n6TqYAu1SYhgYFi7UbYS7BJmlUUDKnMUxzuLyJwVvXfoeKj5CZCnZC75OwQ95mxS8KMsGFJpuFXSZAErpIS5Tc2PGUVM30vofeoy1VltGvYk0y6JWMp4lhCNH7uoUz13IkLBbc1xPiDEZANzwt912XODQZDZD"
+TOKEN = ""
 URL="https://graph.facebook.com/"
 
 with open("./addword.txt","r+") as file:
